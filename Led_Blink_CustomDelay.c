03bf605fd4ff5f7747aa77a69552956de05e30cd
M A. ABISHEK KUMAR <154258313+Abishek-Ma@users.noreply.github.com>
2025-08-21 21:56:03 +0530

    Add files via upload
    Custom Delay Achieved using Systik

---
+++ Led_Blink_CustomDelay.c
@@ -0,0 +1,40 @@
+#include<stdint.h>
+ 
+#define RCC_AHB1ENR (*(volatile unsigned int*)0x40023830)
+#define GPIOA_MODER (*(volatile unsigned int*)0x40020000)
+#define GPIOA_ODR (*(volatile unsigned int*)0x40020014)
+ 
+ 
+#define SYST_CSR (*(volatile unsigned int*)0xE000E010)
+#define SYST_RVR (*(volatile unsigned int*)0xE000E014)
+#define SYST_CVR (*(volatile unsigned int*)0xE000E018)
+ 
+void delay_s(volatile uint32_t);
+ 
+int main()
+{
+	RCC_AHB1ENR |= (1<<0);
+ 
+	GPIOA_MODER &= ~(3<<(5*2));
+	GPIOA_MODER |= (1<<(5*2));
+ 
+	while(1)
+	{
+		GPIOA_ODR ^= (1<<5);
+		delay_s(3);
+	}
+}
+ 
+void delay_s(volatile uint32_t sec)
+{
+	SYST_RVR = 16000000-1;
+	SYST_CVR = 0;
+	SYST_CSR |= ((1<<0) | (1<<2));
+ 
+	for(volatile uint32_t i=0;i<sec;i++)
+	{
+		while((SYST_CSR & (1<<16))==0);
+	}
+ 
+	SYST_CSR=0;
+}
